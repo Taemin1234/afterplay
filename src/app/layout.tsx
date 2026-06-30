f7536c3e1b0b4109cc22e25b8bf1d18d79e24bc6
stm <kingsj1418@gmail.com>
2026-06-30 14:28:17 +0900

    paperlogy 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,46 @@
 import type { Metadata, Viewport } from "next";
 import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
-import localFont from 'next/font/local'
-import { buildDefaultMetadata } from '@/lib/seo';
+import localFont from "next/font/local";
+import { buildDefaultMetadata } from "@/lib/seo";
 
 const pretendard = localFont({
-  src: '../fonts/PretendardVariable.woff2',
-  display: 'swap',
-  variable: '--font-pretendard',
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard",
 });
 
+const paperlogy = localFont({
+  src: [
+    {
+      path: "../fonts/Paperlogy-3Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Paperlogy-4Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Paperlogy-5Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Paperlogy-7Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Paperlogy-8ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-paperlogy",
+});
 
 export const metadata: Metadata = buildDefaultMetadata();
 
@@ -27,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={pretendard.variable}>
+    <html lang="ko" className={`${pretendard.variable} ${paperlogy.variable}`}>
       <GoogleTagManager gtmId={GTM_ID} />
       <body>{children}</body>
     </html>
